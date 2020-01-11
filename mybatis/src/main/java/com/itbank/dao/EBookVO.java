8f1004cb98efd9070b169deda4bc62a76336dfe1
JSY <JSY@192.168.1.1>
2020-01-11 19:08:55 +0900

    다시 보낸다?!!

---
+++ mybatis/src/main/java/com/itbank/dao/EBookVO.java
@@ -0,0 +1,70 @@
+package com.itbank.dao;
+
+public class EBookVO {
+	private int bnum;
+	private String bpicture, btitle, bfield, bwriter, btrans, bpublisher, bdate, bprice, bintro;
+	
+	// EBookVO
+	
+	public int getBnum() {
+		return bnum;
+	}
+	public void setBnum(int bnum) {
+		this.bnum = bnum;
+	}
+	public String getBpicture() {
+		return bpicture;
+	}
+	public void setBpicture(String bpicture) {
+		this.bpicture = bpicture;
+	}
+	public String getBtitle() {
+		return btitle;
+	}
+	public void setBtitle(String btitle) {
+		this.btitle = btitle;
+	}
+	public String getBfield() {
+		return bfield;
+	}
+	public void setBfield(String bfield) {
+		this.bfield = bfield;
+	}
+	public String getBwriter() {
+		return bwriter;
+	}
+	public void setBwriter(String bwriter) {
+		this.bwriter = bwriter;
+	}
+	public String getBtrans() {
+		return btrans;
+	}
+	public void setBtrans(String btrans) {
+		this.btrans = btrans;
+	}
+	public String getBpublisher() {
+		return bpublisher;
+	}
+	public void setBpublisher(String bpublisher) {
+		this.bpublisher = bpublisher;
+	}
+	public String getBdate() {
+		return bdate;
+	}
+	public void setBdate(String bdate) {
+		this.bdate = bdate;
+	}
+	public String getBprice() {
+		return bprice;
+	}
+	public void setBprice(String bprice) {
+		this.bprice = bprice;
+	}
+	public String getBintro() {
+		return bintro;
+	}
+	public void setBintro(String bintro) {
+		this.bintro = bintro;
+	}
+	
+}
